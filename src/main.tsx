e64853b77e2a0289018b5adea7480ccb37d602c2
apexhub201 <luuthinh27112011@gmail.com>
2026-09-06 15:36:30 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+console.log('LUAUFORGE X - Starting...');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+console.log('LUAUFORGE X - Rendered');
